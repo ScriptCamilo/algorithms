5f89842e81e59c561c8adac33f0e07b4db1f3dc3
ScriptCamilo <script.camilo@gmail.com>
2022-10-29 16:12:15 -0300

    pendency: doing backup to finish later

---
+++ gradingStudents.js
@@ -0,0 +1,18 @@
+// https://www.hackerrank.com/challenges/grading/problem
+
+function gradingStudents(grades) {
+  // Write your code here
+  const newGrades = [];
+
+  // for (const grade of grades) {
+
+  //   switch (true) {
+  //     case grade
+  //   }
+  // }
+}
+
+const grades = [73, 67, 38, 33];
+const result = gradingStudents(grades);
+
+console.log(result);
