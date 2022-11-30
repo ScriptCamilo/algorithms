3ae863bdd43ed4bc1dccc4ee5a9f5d9a34a4b951
ScriptCamilo <script.camilo@gmail.com>
2022-11-30 10:32:45 -0300

    feat: completed hackerrank grading problem

--- gradingStudents.js
+++ gradingStudents.js
@@ -4,12 +4,19 @@ function gradingStudents(grades) {
   // Write your code here
   const newGrades = [];
 
-  // for (const grade of grades) {
+  for (const grade of grades) {
+    if (grade > 37) {
+      const multipleNumber = 5;
+      const gradeDiffer = multipleNumber - (grade % multipleNumber);
+      if (gradeDiffer < 3) {
+        newGrades.push(grade + gradeDiffer);
+        continue;
+      }
+    }
+    newGrades.push(grade);
+  }
 
-  //   switch (true) {
-  //     case grade
-  //   }
-  // }
+  return newGrades;
 }
 
 const grades = [73, 67, 38, 33];
