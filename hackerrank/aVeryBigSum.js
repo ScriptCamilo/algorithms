41b7948767b6c80e5bcac86e0a1a2f4da9586ab5
ScriptCamilo <script.camilo@gmail.com>
2023-03-09 22:50:36 -0300

    feat: add hackerrank aVeryBigSum algorithm

---
+++ hackerrank/aVeryBigSum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Array<number>} numberArray
+ * @returns {number}
+ */
+
+function aVeryBigSum(numberArray) {
+  const finalSum = numberArray.reduce((sum, num) => {
+    return sum + num;
+  });
+
+  return finalSum;
+}
+
+const numberArrayExample = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005];
+
+console.log(aVeryBigSum(numberArrayExample));
